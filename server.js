d64c0b1434f6c93fe9dda0496de535c672710873
Joshua Rangai <rangaij0116@hotmail.com>
2022-05-12 17:59:07 -0500

    add untracked files

---
+++ server.js
@@ -0,0 +1,61 @@
+const fs = require('fs');
+const path = require('path');
+const uniqid = require('uniqid');
+let notesFile;
+
+fs.readFile('./db/db.json', 'utf8', function (err, data) {
+    notesFile = JSON.parse(data);
+})
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+
+// use app
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+
+// load css and javascript 
+app.use(express.static('public'));
+
+// load saved notes
+app.get('/api/notes', (req, res) => {
+    res.json(notesFile.notes);
+});
+
+// add new notes
+app.post('/api/notes', (req, res) => {
+    let notesArray =notesFile.notes;
+    req.body.id = uniqid();
+    notesArray.push(req.body);
+    console.log(notesArray)
+    fs.writeFileSync(
+        path.join(__dirname, './db/db.json'),
+        JSON.stringify({ notes: notesArray})
+    )
+    res.json(req.body)
+})
+    
+
+// delete notes
+
+// dirname routes
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+// Wildcard Routes
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+// call server
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}`);
+});
+
+// validate notes
